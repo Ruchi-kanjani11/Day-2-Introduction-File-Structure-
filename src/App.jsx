c70e75b8ca92c18d85262b3bb95c5c59b0bbd0ba
Ruchi-kanjani11 <ruchikanjani05@gmail.com>
2026-03-12 17:15:05 +0530

    upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Counter from "./components/Counter";
+function App(){
+return(
+  <>
+  <h1>Helloo from parent component</h1>
+  <Counter></Counter>
+  </>
+)
+}
+
+export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,8 @@
+function Counter(){
+    return(
+        <>
+        <h1>Helloo from children component.</h1>
+        </>
+    )
+}
+export default Counter;
